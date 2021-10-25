2795878737f8ab3a1e9ee918886d2af86cafbf5c
ibandedios <75307172+ibandedios@users.noreply.github.com>
2021-10-25 22:00:04 +0200

    Add files via upload

--- Activitat1/src/controllers/clearcookie.php
+++ Activitat1/src/controllers/clearcookie.php
@@ -1,8 +1,13 @@
 <?php 
 //limpiar la cookie de guardado de datos para que no se inicie directamente 
 //el mismo usuario
+if (isset($_COOKIE['datosEmail'])){
          if($_COOKIE['datosEmail']!=NULL){
             //te reemplaza los datos como FALSE
             setcookie('datosEmail',FALSE,0,"/","localhost");
             header('Location:?url=home');
          }
+      }
+      else{
+         header('Location:?url=home');
+      }
