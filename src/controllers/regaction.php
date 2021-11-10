307f52644938a6042b8e21cfb149a7e98a01b39b
iban <ibandediosblanco@hotmail.com>
2021-11-10 21:45:47 +0100

    add the ignore file

---
+++ src/controllers/regaction.php
@@ -0,0 +1,24 @@
+<?php
+
+    require 'conn.php';
+    $user=$_POST['user'];
+    $email=$_POST['email'];
+    $tipo=$_POST['tipo'];
+    $passwd=$_POST['pass'];
+    //intentar
+    try{
+    //insertar en la tabla usuarios en los campos email,nombre,password,
+    //los valores de las variables, con passwd encriptado en md5
+    $query = "INSERT into usuarios (email, nombre, passwd, tipo) 
+    VALUES ('$email', '$user', '" . md5($passwd) . "', '$tipo')";
+    $consulta = $gdb->prepare($query);
+    $consulta->execute();
+    //cuando se ejecute todo te manda al home
+    header('Location:?url=home');
+    //si hay algun error porque el email no se puede repetir en la base de datos,
+    //se tiene que repetir el registro
+    }
+    catch(PDOException $e){
+        header('Location:?url=repitereg');
+
+    }
---
+++ src/templates/home.tpl.php
@@ -0,0 +1,14 @@
+<html>
+   
+   <head>
+      <title>Home</title>
+      <link href="/src/templates/estilo.css" type="text/css" rel="stylesheet">
+   </head>
+   
+   <body>
+   <h1><?=$nom;?></h1>
+       <p class="fondoNormal"><a href="?url=login">LOGIN</a></p>
+       <p class="fondoNormal"><a href="?url=register">REGISTER</a></p>
+   </body>
+
+</html>
