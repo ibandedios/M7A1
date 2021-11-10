a15ead9d965a67de06e1f826c6cd06820f0f1fa5
ibandedios <75307172+ibandedios@users.noreply.github.com>
2021-11-10 23:10:38 +0100

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//DATOS acceso a BBDD
-
-$dbhost=$_ENV['DDBB_HOST'];
-$dbname=$_ENV['DB_NAME'];
-$dbuser=$_ENV['DDBB_USER'];
-$dbpasswd=$_ENV['DDBB_PASSWORD'];
-$dsn='mysql:host='.$dbhost.';dbname='.$dbname. ';charset=utf8mb4';
-
